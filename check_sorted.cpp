038cb9ea147be6a574ce14afe0daf7ef458e7733
ashish095-EE <127418703+ashish095-EE@users.noreply.github.com>
2024-01-28 19:41:56 +0530

    Add files via upload

---
+++ check_sorted.cpp
@@ -0,0 +1,13 @@
+int isSorted(int n, vector<int> a) {
+    for(int i= 0;i<a.size();i++){
+        if(a[i]>=a[i-1]){
+
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
+        
+        
+}
